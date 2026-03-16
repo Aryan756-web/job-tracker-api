c26037eebf158c9ee2b15bb4184be8c8227cc568
Aryan756-web <aryankaradkar2312@gmail.com>
2026-03-16 19:14:27 +0530

    updated route prefixes

--- src/server.js
+++ src/server.js
@@ -18,8 +18,8 @@ app.get("/", (req, res) => {
   res.send("Job Tracker API running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
